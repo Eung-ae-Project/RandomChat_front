2ce75aecae6bbcf74b0e5360c427af5431cd938e
smg0725 <smg0725@naver.com>
2024-03-04 19:20:52 +0900

    createChatMenu

--- src/components/CreateChat/CreateChat.tsx
+++ src/components/CreateChat/CreateChat.tsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import styles from "@components/CreateChat/qwer.module.css";
 
 
+
 const CreateChat = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const onToggleMenu = () => {
-		setIsOpen(!isOpen);
+	const backgroundRef = useRef(null);
+	const onToggleMenu = (e:any) => {
+		if(backgroundRef.current == e.target){
+			setIsOpen(false)
+		}else{
+			setIsOpen(true)
+		}
 	};
 
 	return (
@@ -16,10 +22,26 @@ const CreateChat = () => {
 				채팅방 생성
 				<FontAwesomeIcon icon={faPlus} />
 			</button>
-			{isOpen && <div className={styles.background} onClick={onToggleMenu}>
+			{isOpen && <div ref={backgroundRef} className={styles.background} onClick={onToggleMenu}>
 				<div className={styles.modal}>
 					<div className={styles.chatImage}>
-						<img src="src/assets/profile.png"  alt="chatImage"></img>
+						<img src="src/assets/profile.png" alt="chatImage"></img>
+						<form>
+							<input type="text" placeholder="채팅방 이름" />
+							<input type="text" placeholder="채팅방 설명" />
+							<input type="text" placeholder="태그" />
+							<select>
+								<option value="20">20</option>
+								<option value="50">50</option>
+								<option value="100">100</option>
+							</select>
+							<input type="radio" value="공개" />
+							<input type="radio" value="비공개" />
+							<input type="number"/>
+							<button>채팅방 생성</button>
+						</form>
+
+
 					</div>
 				</div>
 			</div>}
